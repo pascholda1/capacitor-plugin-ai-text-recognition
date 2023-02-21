38f6a2dceb28fc827f6b83cbaf3c8568c7d5549f
Simon Paschold <s.paschold@pixelstein.de>
2023-02-21 19:12:46 +0100

    replaced MLKit with Vision framework

--- ios/Plugin/CapacitorPluginMlKitTextRecognitionPlugin.swift
+++ ios/Plugin/CapacitorPluginMlKitTextRecognitionPlugin.swift
@@ -1,87 +1,102 @@
 import Foundation
 import Capacitor
-import MLKitVision
-import MLKitTextRecognition
+import CoreGraphics
+import Vision
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
-@objc(CapacitorPluginMlKitTextRecognitionPlugin)
-public class CapacitorPluginMlKitTextRecognitionPlugin: CAPPlugin {
+@objc(CapacitorPluginAITextRecognitionPlugin)
+public class CapacitorPluginAITextRecognitionPlugin: CAPPlugin {
+
+    var _call:CAPPluginCall!
+
     @objc func detectText(_ call: CAPPluginCall) {
+        self._call = call;
+
         guard let encodedImage = call.getString("base64Image") else {
             call.reject("No image is given!")
             return
         }
-        let rotation = call.getInt("rotation") ?? 0
+        let r = call.getInt("rotation") ?? 0
+        let rotation = UInt32(r)
+        guard let orientation = CGImagePropertyOrientation.init(rawValue: rotation) else { return  }
 
         let dataDecoded : Data = Data(base64Encoded: encodedImage, options: .ignoreUnknownCharacters)!
-        guard let image = UIImage(data: dataDecoded) else {
+        guard let image = UIImage(data: dataDecoded)?.cgImage else {
             call.reject("Unable to parse image")
             return
         }
 
-        let latinOptions = TextRecognizerOptions()
-        let textRecognizer = TextRecognizer.textRecognizer(options: latinOptions)
-        let visionImage = VisionImage(image: image)
-        visionImage.orientation = visionImageOrientation(rotation: rotation)
 
-        textRecognizer.process(visionImage) { result, error in
-          guard error == nil, let result = result else {
-            // Error handling
-            call.reject("Error on processing image")
-            return
-          }
+        let request = VNRecognizeTextRequest(completionHandler: self.handleDetectedText)
+        request.recognitionLevel = .accurate
 
-          let textBlocks = NSMutableArray()
-          for textBlock: TextBlock in result.blocks {
-            let lines = NSMutableArray()
-            for line: TextLine in textBlock.lines {
-              let elements = NSMutableArray()
-              for element: TextElement in line.elements {
-                elements.add([
-                  "text": element.text,
-                  "boundingBox": [
-                    "left": element.frame.minX,
-                    "top": element.frame.maxY,
-                    "right": element.frame.maxX,
-                    "bottom": element.frame.minY,
-                  ],
-                  "recognizedLanguage": element.recognizedLanguages.first?.languageCode ?? ""
-                ])
-              }
 
-              lines.add([
-                "text": line.text,
-                "boundingBox": [
-                    "left": line.frame.minX,
-                    "top": line.frame.maxY,
-                    "right": line.frame.maxX,
-                    "bottom": line.frame.minY,
-                ],
-                "recognizedLanguage": line.recognizedLanguages.first?.languageCode ?? "",
-                "elements": elements
-              ])
+        let requests = [request]
+        let imageRequestHandler = VNImageRequestHandler(cgImage: image, orientation: orientation, options: [:])
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try imageRequestHandler.perform(requests)
+            } catch let error {
+                print("Error: \(error)")
+                call.reject("Error: \(error)")
+
             }
-
-            textBlocks.add([
-               "text": textBlock.text,
-               "boundingBox": [
-                 "left": textBlock.frame.minX,
-                 "top": textBlock.frame.maxY,
-                 "right": textBlock.frame.maxX,
-                 "bottom": textBlock.frame.minY,
-               ],
-               "recognizedLanguage": textBlock.recognizedLanguages.first?.languageCode ?? "",
-               "lines": lines
-             ])
-          }
-
-          call.resolve([
-            "text": result.text,
-            "blocks": textBlocks
-          ])
         }
+    }
+
+    func handleDetectedText(request: VNRequest?, error: Error?) {
+        if let error = error {
+            print("ERROR: \(error)")
+            return
+        }
+        guard let results = request?.results, results.count > 0 else {
+            print("No text found")
+            return
+        }
+
+        let textBlocks = NSMutableArray()
+        var allText = ""
+
+        for result in results {
+            if let observation = result as? VNRecognizedTextObservation {
+                for text in observation.topCandidates(1) {
+                    
+                    
+                    textBlocks.add([
+                        "text": text.string,
+                        "boundingBox": [
+                            "left": text.accessibilityFrame.minX,
+                            "top": text.accessibilityFrame.maxY,
+                            "right": text.accessibilityFrame.maxX,
+                            "bottom": text.accessibilityFrame.minY,
+                        ],
+                        "recognizedLanguage": text.accessibilityLanguage ?? "",
+                        "lines": NSMutableArray(object: [
+                            "text": text.string,
+                            "boundingBox": [
+                                "left": text.accessibilityFrame.minX,
+                                "top": text.accessibilityFrame.maxY,
+                                "right": text.accessibilityFrame.maxX,
+                                "bottom": text.accessibilityFrame.minY,
+                            ],
+                            "recognizedLanguage": text.accessibilityLanguage ?? ""
+                        ])
+                      ])
+
+                    allText = allText + " " + text.string
+
+                }
+            }
+        }
+
+
+        self._call.resolve([
+            "text": allText,
+          "blocks": textBlocks
+        ])
+
     }
 
     public func visionImageOrientation(rotation: Int) -> UIImage.Orientation {
